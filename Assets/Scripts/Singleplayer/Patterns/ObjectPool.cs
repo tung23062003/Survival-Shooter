533a994106cacf9e87e4c6753276eb74e1e8e8cc
TungNguyen <tungnguyenviet23062003@gmail.com>
2025-02-14 14:46:00 +0700

    update object pool

--- Assets/Scripts/Singleplayer/Patterns/ObjectPool.cs
+++ Assets/Scripts/Singleplayer/Patterns/ObjectPool.cs
@@ -2,17 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectPool : MonoBehaviour
+public class ObjectPool : Singleton<ObjectPool>
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    Dictionary<GameObject, List<GameObject>> _listOjects = new Dictionary<GameObject, List<GameObject>>();
 
-    // Update is called once per frame
-    void Update()
+    public GameObject GetObject(GameObject defaultPref)
     {
-        
+        if (_listOjects.ContainsKey(defaultPref))
+        {
+            foreach (GameObject g in _listOjects[defaultPref])
+            {
+                if (g.activeSelf)
+                    continue;
+                return g;
+            }
+            GameObject b = Instantiate(defaultPref, this.transform.position, Quaternion.identity);
+            _listOjects[defaultPref].Add(b);
+            b.SetActive(false);
+
+            return b;
+        }
+        List<GameObject> _newList = new List<GameObject>();
+
+        GameObject o = Instantiate(defaultPref, this.transform.position, Quaternion.identity);
+        _newList.Add(o);
+        o.SetActive(false);
+        _listOjects.Add(defaultPref, _newList);
+
+        return o;
     }
 }
